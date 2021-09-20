927d707547a4583f69273cfafdb968e05959a5af
julian <juka1031@gmail.com>
2021-09-19 22:07:22 -0400

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require("express")
 const app = express();
+const mongoose = require("mongoose")
+const db = require("./confg/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require('passport');
 
+
+mongoose
+    .connect(db,{useNewUrlParser:true})
+    .then(()=>console.log("Connected to mongoDB"))
+    .catch(err=>console.log(err))
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 app.get("/", (req,res)=> {
-    res.send("Hello World!!!!")
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    })
+    user.save();
+    res.send("Hello Worl!")
 });
+app.use(passport.initialize());
+require('./confg/passport')(passport)
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
 
 
 const port = process.env.PORT || 5000;
