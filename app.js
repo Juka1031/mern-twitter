92b869973e2fbf2aabd20d11eedd12286e921433
julian <juka1031@gmail.com>
2021-09-19 23:39:45 -0400

    pushing to heroku

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
@@ -7,6 +8,13 @@ const tweets = require("./routes/api/tweets");
 const User = require("./models/User");
 const bodyParser = require("body-parser");
 const passport = require('passport');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
 
 
 mongoose
---
+++ confg/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports={
+    mongoURI:"mongodb+srv://admin:Z4pTjEFAy9FiXO94@mern.xkkqe.mongodb.net/mern-twitter=true&w=majority",
+    secretOrKey: "secret"
+}
